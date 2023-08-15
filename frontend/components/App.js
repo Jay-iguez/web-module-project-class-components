fc00aa96d750c06216001315be4464e8f9bb5050
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-08-15 16:35:29 -0700

    Npm install styled-components and create state array

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,38 @@
 import React from 'react'
 
+const itemsList = [
+  {
+    name: 'Clean room',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    name: 'Walk dog',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    name: 'Buy more cheese',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    name: 'Buy lock to keep cheese safe from dog',
+    id: Date.now(),
+    completed: false
+  }
+]
+
+console.log(itemsList)
+
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      itemsList: itemsList
+    }
+  }
+
   render() {
     return (
       <div>
